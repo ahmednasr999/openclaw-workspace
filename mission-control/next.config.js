a773aa33ca608d9b23215b0da9a9ac369935ca6f
Ahmed Nasr <ahmednasr999@gmail.com>
2026-02-19 08:30:45 +0000

    auto-backup: 2026-02-19 08:30

--- mission-control/next.config.js
+++ mission-control/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Convex project ID is loaded from .env.local
-  // NEXT_PUBLIC_CONVEX_PROJECT_ID environment variable
+  // Convex configuration is handled separately
 }
 
 module.exports = nextConfig
